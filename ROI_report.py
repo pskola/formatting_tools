b420d14d02d407d79809ac481d04955d8626313a
pskola <48459258+pskola@users.noreply.github.com>
2020-02-29 12:39:54 -0600

    report featquery mean now overwrites the file with only the relevant values and adds file name in the first row

---
+++ ROI_report.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#extract third float from ROI report file
+#Usage: python ./ROI_report_mean.py <filename>
+
+import glob, os
+
+for file in glob.glob("*.txt"):
+	with open(file, "r+") as f:
+		data = f.read()
+		f.seek(0)
+		tex=data.split("\n")
+		te=[line.split(" ") for line in tex]
+		vals=[te[i][5] for i in range(len(te)-1)]
+		f.write(file)
+		for t in vals:
+			f.write(t)
+			f.write("\n")
+		f.truncate()
+
--- ROI_report_mean.py
+++
@@ -1,1 +0,0 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-#extract third float from fsl's ROI report file#Usage: python ./ROI_report_mean.py <filename>import sysfile=open(sys.argv[1],"r")text=file.read()file.close()tex=text.split("\n")te=[line.split(" ") for line in tex]vals=[te[i][5] for i in range(len(te)-1)]for t in vals:    print(t)
